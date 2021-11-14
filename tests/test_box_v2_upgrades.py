28e427274d8049ab66d7259a54808a644f4936db
juliocslima <lima.julio.cs@gmail.com>
2021-11-14 13:48:08 -0300

    Upgrade contract and tests

--- tests/test_box_v2_upgrades.py
+++ tests/test_box_v2_upgrades.py
@@ -1,34 +1,29 @@
-from scripts.helpful_scripts import get_account, encode_function_data, upgrade
+from scripts.helpful_scripts import encode_function_data, get_account, upgrade
 from brownie import (
     Box,
     BoxV2,
     ProxyAdmin,
     TransparentUpgradeableProxy,
-    network,
     Contract,
-    config
+    config,
+    network
 )
+import pytest
 
 
-def main():
+def test_box_v2_upgrades():
     account = get_account()
-    print(f"Deploying to {network.show_active()}")
     box = Box.deploy(
         {"from": account},
         publish_source=config["networks"][network.show_active()].get(
             "verify", False),
     )
-
     proxy_admin = ProxyAdmin.deploy(
         {"from": account},
         publish_source=config["networks"][network.show_active()].get(
             "verify", False),
     )
-
-    initializer = box.store, 1
-    box_encoded_initializer_function = encode_function_data(
-        initializer=initializer)
-
+    box_encoded_initializer_function = encode_function_data()
     proxy = TransparentUpgradeableProxy.deploy(
         box.address,
         proxy_admin.address,
@@ -37,14 +32,17 @@ def main():
         publish_source=config["networks"][network.show_active()].get(
             "verify", False),
     )
-
-    print(f"Proxy deployed to {proxy}, you can now upgrade to v2!")
     proxy_box = Contract.from_abi("Box", proxy.address, Box.abi)
-    print(proxy_box.retrieve())
+
+    assert proxy_box.retrieve() == 0
+
     proxy_box.store(1, {"from": account})
-    print(proxy_box.retrieve())
+    assert proxy_box.retrieve() == 1
 
     # Upgrade contract Box to BoxV2
+    # with pytest.raises(exceptions.VirtualMachineError):
+    #    proxy_box.increment({"from": account})
+
     box_v2 = BoxV2.deploy(
         {"from": account},
         publish_source=config["networks"][network.show_active()].get(
@@ -54,8 +52,7 @@ def main():
         account, proxy, box_v2.address, proxy_admin_contract=proxy_admin)
     upgrade_transaction.wait(1)
 
-    print("Proxy has been upgraded!")
-
     proxy_box = Contract.from_abi("BoxV2", proxy.address, BoxV2.abi)
-    print(proxy_box.increment({"from": account}))
-    print(proxy_box.retrieve())
+
+    proxy_box.increment({"from": account})
+    assert proxy_box.retrieve() == 2
