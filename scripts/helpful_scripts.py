0fcee1c5d9237ef926b8e31d29961c6970b8c47b
juliocslima <lima.julio.cs@gmail.com>
2021-11-14 12:18:29 -0300

    Deploy and upgrade Box contract with proxy

---
+++ scripts/helpful_scripts.py
@@ -0,0 +1,34 @@
+from brownie import (
+    network,
+    accounts,
+    config,
+)
+import eth_utils
+
+NON_FORKED_LOCAL_BLOCKCHAIN_ENVIRONMENTS = [
+    "hardhat", "development", "ganache"]
+
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = NON_FORKED_LOCAL_BLOCKCHAIN_ENVIRONMENTS + [
+    "mainnet-fork",
+    "binance-fork",
+    "matic-fork",
+]
+
+
+def get_account(index=None, id=None):
+    if index:
+        return accounts[index]
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        return accounts[0]
+    if id:
+        return accounts.load(id)
+    if network.show_active() in config["networks"]:
+        return accounts.add(config["wallets"]["from_key"])
+    return None
+
+
+# initializer=box.store, 1
+def encode_function_data(initializer=None, *args):
+    if len(args) == 0 or not initializer:
+        return eth_utils.to_bytes(hexstr="0x")
+    return initializer.encode_input(*args)
